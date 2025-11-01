09b81381c5773bf344ac035a5eca5570165ca3a1
Alwin224 <alwinbhogal2@gmail.com>
2025-11-01 03:56:02 +0100

    Changed all the files that were old and started because vercel messed it up

--- mushroomhouse.js
+++ mushroomhouse.js
@@ -3,7 +3,6 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { gsap } from 'gsap';
-import { sRGBEncoding } from 'three';
 
 
 const loadingScreen = document.getElementById('loadingScreen');
